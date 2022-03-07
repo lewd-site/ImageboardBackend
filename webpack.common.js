09fe38018bb3cb7c5427ef4aefeaf71ecfbe8b78
Nina Lisitsinskaya <vvatashi@protonmail.com>
2022-03-07 09:34:41 +0600

    Create project

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.ts', '...'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [new webpack.ProgressPlugin()],
+};
